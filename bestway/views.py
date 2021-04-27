6d51ef81bcc669cad220381f97bc5616c1951298
thomas <psychoid@hotmail.fr>
2021-04-27 14:34:20 +0200

    add home(request) & home.html

--- bestway/views.py
+++ bestway/views.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import requests
+
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+
+    return render(request, 'home.html')
