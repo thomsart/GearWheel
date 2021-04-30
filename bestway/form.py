c227f650a49fcd28b34e9480235231e5edaf1a19
thomas <psychoid@hotmail.fr>
2021-04-30 16:48:47 +0200

    recuperation addresses dans vue

--- bestway/form.py
+++ bestway/form.py
@@ -1,2 +1,6 @@
-from django import form
+from django import forms
 
+
+class AddressForm(forms.Form):
+    address_start = forms.CharField(label="Adresse de départ", max_length=70)
+    address_end = forms.CharField(label="Adresse d'arrivée", max_length=70)
--- bestway/views.py
+++ bestway/views.py
@@ -4,12 +4,29 @@
 import requests
 
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+from bestway.form import *
 
 # Create your views here.
 
 def home(request):
-    
-    return render(request, 'home.html')
+
+    if request.method == 'POST':
+        address_form = AddressForm(request.POST)
+        if address_form.is_valid():
+            addresses = [
+                address_form.cleaned_data['address_start'],
+                address_form.cleaned_data['address_end']
+            ]
+            print(addresses)
+
+            return HttpResponseRedirect('')
+
+    else:
+        address_form = AddressForm()
+
+    return render(request, 'home.html', {"address_form": address_form})
 
 
 def mentions_legales(request):
