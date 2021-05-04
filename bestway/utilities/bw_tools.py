f97fb86178a76180e5b10a287369c0f86d9b2064
thomas <psychoid@hotmail.fr>
2021-05-04 17:25:45 +0200

    add create_json()

--- bestway/utilities/bw_tools.py
+++ bestway/utilities/bw_tools.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+
 import requests
+import json
 
 """
     This file contains all functions we need in the views.
@@ -9,4 +12,33 @@ import requests
 ################################################################################
 #####                               Tools                                  #####
 ################################################################################
+
+def create_json():
+
+    quantity_of_json = 0
+
+    for file in os.listdir("json"):
+        quantity_of_json += 1
+
+    datas = {
+        "address": "",
+        "longitude": 0,
+        "latitude": 0
+    }
+
+    with open("json/stop"+str(((quantity_of_json - 3)+1))+".json", "w") as file:
+        json.dump(datas, file)
+
+    return
+
+def main():
+
+    create_json()
+
+    return
+
+if __name__ == "__main__":
+
+    main()
+
 
