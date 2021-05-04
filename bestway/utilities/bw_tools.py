a88031249d30acc9bffefa2c72272c573f11db6b
thomas <psychoid@hotmail.fr>
2021-05-04 19:03:44 +0200

    erreur de conception

--- bestway/utilities/bw_tools.py
+++ bestway/utilities/bw_tools.py
@@ -13,30 +13,6 @@ import json
 #####                               Tools                                  #####
 ################################################################################
 
-def create_json():
-
-    quantity_of_json = 0
-
-    for file in os.listdir("json"):
-        quantity_of_json += 1
-
-    datas = {
-        "address": "",
-        "longitude": 0,
-        "latitude": 0
-    }
-
-    with open("json/stop"+str(((quantity_of_json - 3)+1))+".json", "w") as file:
-        json.dump(datas, file)
-
-    return
-
-def main():
-
-    create_json()
-
-    return
-
 if __name__ == "__main__":
 
     main()
