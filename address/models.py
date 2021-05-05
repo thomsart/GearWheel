812dc186db3dd5dfc752696744a88ff8b0c8c52d
thomas <psychoid@hotmail.fr>
2021-05-05 10:20:23 +0200

    blank not blanck

--- address/models.py
+++ address/models.py
@@ -5,9 +5,9 @@ from ... import User
 # Create your models here.
 
 class Address(models.Model):
-    name = models.CharField(blanck=False, max_length=70)
-    longitude = models.DecimalField(blanck=False, max_digits=20, decimal_places=17)
-    latitude = models.DecimalField(blanck=False, max_digits=20, decimal_places=17)
+    name = models.CharField(blank=False, max_length=70)
+    longitude = models.DecimalField(blank=False, max_digits=20, decimal_places=17)
+    latitude = models.DecimalField(blank=False, max_digits=20, decimal_places=17)
     start = models.BooleanField(default=False)
     end = models.BooleanField(default=False)
     stop = models.BooleanField(default=False)
