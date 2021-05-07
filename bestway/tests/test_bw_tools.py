d50da60d75cef93501edfeaee730544fbd9b5c0e
thomas <psychoid@hotmail.fr>
2021-05-07 17:56:58 +0200

    bw_tools+test ok

--- bestway/tests/test_bw_tools.py
+++ bestway/tests/test_bw_tools.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*
 
+import pytest
 
+from django.test import TestCase
+
+from bestway.utilities.bw_tools import * 
 
 """
     This file contains all tests of the functions in bw_tools.py.
@@ -10,3 +14,34 @@
 #####                               Tests                                  #####
 ################################################################################
 
+class TestViews(TestCase):
+
+    def  setUp(self):
+        """ We defined here all the datas we need to do our tests. """
+
+        self.good_string = "186 rue du Faubourg Saint Antoine 75012 Paris"
+
+        self.upper_string = "186 RUE DU FAUBOURG SAINT ANTOINE 75012 PARIS"
+
+        self.symbole_string = "'èäâê%?!^¨$,/._#²éù~µ€><£¤``()}{[]-*ç;§@àé&"
+
+        self.empty_string = ""
+
+    def test_clean_address(self):
+        """
+            Here we test
+        """
+
+        assert clean_address(self.good_string) == "186+rue+du+faubourg+saint+antoine+75012+paris"
+        assert clean_address(self.upper_string) == "186+rue+du+faubourg+saint+antoine+75012+paris"
+        assert clean_address(self.symbole_string) == "'++++++++++++++++++++++++++++++++++++++++++"
+        assert clean_address(self.empty_string) == ""
+
+    # def request_to_GeoApiGouvFr(self):
+    #     """
+    #         Here we test 
+    #     """
+
+    #     assert tools.put_products_in_db(self.dict_without_product) == False
+
+################################################################################
--- bestway/utilities/bw_tools.py
+++ bestway/utilities/bw_tools.py
@@ -13,35 +13,46 @@ import requests
 #####                               Tools                                  #####
 ################################################################################
 
-BESTWAY_GOOGLEMAP_KEY = os.environ['BESTWAY_GOOGLEMAP_KEY']
+def clean_address(address):
 
-def main():
-
-    keep_caracters = [
+    """
+        This function formate the address that the user enter in the form in
+        oder to make the requests success in the API. 
+    """
+    caracters_to_keep = [
         '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h',
         'i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
         "'"
-        ]
-    address = input("le point de départ : \n")
+    ]
     address = address.lower()
     for caracter in address:
-        if caracter in keep_caracters:
+        if caracter in caracters_to_keep:
             continue
         else:
             address = address.replace(caracter, "+")
 
-    print(address)
+    return address
 
+################################################################################
+
+def request_to_GeoApiGouvFr(address):
+
+    """
+        This function will request to 'https://geo.api.gouv.fr/adresse',
+        in order to get the good syntaxe of the address and its coorinates.
+    """
     response = requests.get("https://api-adresse.data.gouv.fr/search/?q="+address)
-    
-    print("Adresse : "+response.json()['features'][0]['properties']['label'])
-    print("Longitude : "+str(response.json()['features'][0]['geometry']['coordinates'][0]))
-    print("Latitude : "+str(response.json()['features'][0]['geometry']['coordinates'][1]))
 
-    return
+    address = response.json()['features'][0]['properties']['label']
+    longitude = str(response.json()['features'][0]['geometry']['coordinates'][0])
+    latitude = str(response.json()['features'][0]['geometry']['coordinates'][1])
 
-if __name__ == "__main__":
+    return {
+        "address": address,
+        "longitude": longitude,
+        "latitude": latitude
+    }
 
-    main()
+################################################################################
 
 
