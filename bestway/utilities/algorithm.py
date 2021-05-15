af66ed6d0803f85746011b3dca848627328c2c6d
thomas <psychoid@hotmail.fr>
2021-05-15 16:31:36 +0200

    no comment

--- bestway/utilities/algorithm.py
+++ bestway/utilities/algorithm.py
@@ -6,36 +6,6 @@ from itertools import permutations
 
 
 """
-Le but de l'algorithme est de trouver le chemin le moins couteux en terme de
-distance en prenant en considération tout les arrêts demandés.
-
-l'idée est la suivante:
-On doit en realité réussir à faire le polygone qui possède le moins de côtés
-avec tout les sommets demandés, ce qui en fait revient à vouloir avoir la forme
-géométrique qui possède le plus petit périmètre pour le nombre de sommets
-demandé. Pour ce faire il faut procéder comme suit:
-
-1 - Ressencer dans une liste tout les cerlcles du plus grand au plus petit
-possèdant le même centre (notre point de départ).
-
-2 - On réalise ensuite un triangle que l'on matérialisera par une liste des
-trois premiers points de la liste initale dont les trois sommets sont confondus
-avec les trois premiers cercles de notre liste de départ. On désignera cette
-liste comme la liste de fin. On retire donc les trois premiers points de la
-liste de départ.
-
-3 - On prend ensuite le premier point de la liste de départ (qui était le 4ème)
-et on réalise le cercle le plus petit ayant pour centre ce point avec le point
-de la liste obtenue grace à notre triangle. On regarde ensuit le cercle qui est
-juste après en terme de grandeur. Une fois les cercles obtenue on place
-ce quatrième point extrait de la liste de départ pour le placer dans la liste
-de fin entre les deux points qui ont servis à tracer les deux cercles précédants.
-
-4 - On prend ensuite le premier point de la liste de départ (qui était le 5ème)
-pour repeter la même opération jusqu'à épuisement des points de la liste de
-départ. On obtient donc, grâce à notre liste de fin si on traçait les
-droites qui relient dans l'ordre des points, ce polygone qui possède pour sommets
-tout ces points de la liste et ayant le périmètre le plus petit.
 
 """
 
@@ -101,28 +71,27 @@ def calculate_total_distance(addresses_list):
         longitude = 0 et latitude = 0
     """
 
-    ref_longitude = []
-    ref_latitude = []
+    original_addresses_list = addresses_list.copy()
 
-    for addresses_block in addresses_list:
-        del ref_longitude[:]
-        del ref_latitude[:]
-        for address in addresses_block:
-            if address['start'] == True:
-                ref_longitude.append(address['longitude'])
-                ref_latitude.append(address['latitude'])
-            else:
-                ref_longitude.append(address['longitude'])
-                ref_latitude.append(address['latitude'])
-                address['longitude'] -= ref_longitude[-2]
-                address['latitude'] -= ref_latitude[-2]
-                address['distance'] = math.sqrt(
-                                        (address['longitude']*address['longitude']) + (address['latitude']*address['latitude'])
-                                        )
-        
+    for original_way in original_addresses_list:
+        ref_longitude = 0
+        ref_latitude = 0
+        way_indice = original_addresses_list.index(original_way)
+        for address in original_way:
+            ref_longitude = address['longitude']
+            ref_latitude = address['latitude']
+
+            for way in addresses_list:
+                if addresses_list.index(way) == way_indice:
+                    for address in way:
+                        if address['start'] == False:
+                            address['longitude'] -= ref_longitude
+                            address['latitude'] -= ref_latitude
+                            address['distance'] = math.sqrt(
+                                                    (address['longitude']*address['longitude']) + (address['latitude']*address['latitude'])
+                                                    )
 
     return addresses_list
-
 ################################################################################
 
 def total_distance_for_each_way(addresses_list):
