9da2ab99ff7c9b7366f60cd6241b2860959f8ed7
thomas <psychoid@hotmail.fr>
2021-05-27 19:33:49 +0200

    responsive 70%

--- bestway/form.py
+++ bestway/form.py
@@ -26,11 +26,11 @@ class AddressForm(forms.Form):
     """
         The form of the start and end addresses.
     """
-    start = forms.CharField(label="Départ ", max_length=70)
-    end = forms.CharField(label="Arrivée ", max_length=70)
+    start = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': "Adresse de départ"}), max_length=70)
+    end = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': "Adresse d'arrivée"}), max_length=70)
 
 class StopsForm(forms.Form):
     """
         The form of the stops and addresses.
     """
-    stops = forms.CharField(label="Rentre tes arrêts ", max_length=70)
+    stops = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': "Adresse d'arrêt"}), max_length=70)
